3571976f3be545eda819aa3cf3b046da33ec4482
kartowka <antonfley@gmail.com>
2019-12-26 00:34:50 +0200

    filter done

--- EX3/ex1.py
+++ EX3/ex1.py
@@ -1,15 +1,17 @@
 import os
 mydict={}
-for file in os.listdir("/home/kartowka/Desktop/EX3/txts"):
+unsupported_char=['-']
+for file in os.listdir(os.getcwd()+'/txts'):
     """take the names and input data to dictionary"""
     if file.endswith(".txt"):
         file_name_value=str(file[:-4])
-        f=open("/home/kartowka/Desktop/EX3/txts/"+file,'r')
+        f=open(os.getcwd()+'/txts/'+file,'r')
         list=[]
         for line in f:
-            list.extend(line.lower().split())
-        #list_to_set=set(list)
-        #print(list_to_set)
+            if [ele for ele in unsupported_char if(ele in line)]:
+                pass
+            else:
+                list.extend(line.lower().split())
         for _,key in enumerate(list):
             mydict.setdefault(key,[]).append(file_name_value)
         f.close()
@@ -17,8 +19,6 @@ for key,val in mydict.items():
     convert_to_set=set()
     convert_to_set=set(val)
     mydict[key]=convert_to_set
-print(mydict["orange"])
-f=open("/home/kartowka/Desktop/EX3/dictionary.txt",'w')
-for k, v in mydict.items():
-    f.write(str(k)+' '+str(v)+'\n')
+f=open(os.getcwd()+'/dictionary.txt','w')
+f.write(str(mydict))
 f.close()
