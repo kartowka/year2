c204592097e05bb79c2d75c54579bc748c8e64e7
kartowka <antonfley@gmail.com>
2019-12-26 23:11:47 +0200

    fixed ex2

--- EX3/ex1.py
+++ EX3/ex1.py
@@ -4,17 +4,18 @@ files_list=['cities.txt','colors.txt','fruit.txt','names.txt']
 for file in os.listdir(os.getcwd()):
     """take the names and input data to dictionary"""
     if file in files_list:
-        print(file)
         file_name_value=str(file[:-4])
         f=open(os.getcwd()+'/'+file,'r')
-        list=[]
+        list_of_values=[]
         for line in f:
-            list.extend(line.lower().split())
-        for _,key in enumerate(list):
+            list_of_values.extend(line.lower().split())
+        #list1=[]
+        #list1=list(filter(lambda name:name.isalpha(),list2))
+        for _,key in enumerate(list_of_values):
             if not key.isalpha():
-                pass
+               pass
             else:
-                mydict.setdefault(key,set()).add(file_name_value)
+               mydict.setdefault(key,set()).add(file_name_value)
         f.close()
 f=open(os.getcwd()+'/dictionary.txt','w')
 f.write(repr(mydict))
--- EX3/ex2.py
+++ EX3/ex2.py
@@ -1,19 +1,36 @@
 import os
 import re
+from collections import Counter
+#txt open
+f_txt=open(os.getcwd()+'/'+'text.txt','r')
+to_list=[]
+for line in f_txt:
+    a=re.split('[.,!?' '\n1-9]',line.lower())
+    to_list.extend(a)
+s=set(to_list)
+s.remove('')
+f_txt.close()
+
+
+# dictionary open
+f_dict=open(os.getcwd()+'/'+'dictionary.txt','r')
 mydict={}
-files_list=["text.txt"]#,"dictionary.txt"]
-unsupported_chars=[',','.','?','!','\n']
-for file in os.listdir(os.getcwd()):
-    if file in files_list:
-        f=open(os.getcwd()+'/'+file,'r')
-        to_list=[]
-        for line in f:
-            a=re.split('[.,!?' '\n]',line.lower())
-            #to_list.extend(a)
-            to_list.extend(a)
-        s=set(to_list)
-        s.remove('')
-        #newlist=list(filter(lambda i:i is not unsupported_chars,to_list))
-        f.close()
-        print(s)
-        #print(newlist)
+for line in f_dict:
+    mydict=eval(line)
+f_dict.close()
+
+def check_if_exist(tuple_list,first,second):
+    temp_list=second.split()
+    for _,key in enumerate(temp_list):
+        if key in first:
+            tuple_list.append((key,temp_list.count(key),first[key]))
+        else:
+            tuple_list.append((key,temp_list.count(key),{}))
+
+f_words=open(os.getcwd()+'/'+'words.txt','w')
+tuple_list=[]
+for line in s:
+    check_if_exist(tuple_list,mydict,line)
+tuple_list.sort(reverse=True)
+f_words.write(repr(tuple_list))
+f_words.close()
