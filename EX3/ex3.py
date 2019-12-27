e60e636d788ebe85ec766c47d016d876b6cf0ef3
kartowka <antonfley@gmail.com>
2019-12-27 19:18:06 +0200

    finished ex3

--- EX3/ex3.py
+++ EX3/ex3.py
@@ -1,14 +1,21 @@
 import os
 import inflect
-
+"""to use this module please download pip install inflect
+    Example answer: 'the' because it appears at least eleven times.
+    this module change integer to a word"""
 with open(os.getcwd()+'/'+'words.txt') as f:
     text=f.read()
 f.close()
 mylist=eval(text)
-
+at_least_appears=5
+at_least_belongs=1
 print(type(mylist))
 p = inflect.engine()
 for item in mylist:
-    if item[1]>=5:
-        print("{} because it belongs to at least {} categories".format(item[0],p.number_to_words(len(item[2]))))
-        #return ("the solution was found after {} iteration: {}".format(min_iter,x0))
+    if len(item[2])>at_least_belongs:
+        if item[1]>=at_least_appears:
+            print("'{}' because it belongs to at least {} categories and because it appears at least {} times.".format(item[0],p.number_to_words(len(item[2])),p.number_to_words(item[1])))
+        else:
+            print("'{}' because it belongs to at least {} categories.".format(item[0],p.number_to_words(len(item[2]))))
+    elif item[1]>=at_least_appears:
+        print("'{}' because it appears at least {} times.".format(item[0],p.number_to_words(item[1])))
