bf0db1de338531f42d30329956ea3b7095a0ef56
kartowka <antonfley@gmail.com>
2019-12-30 21:43:43 +0200

    fix ex3

--- EX3/HW3_3_203192331.py
+++ EX3/HW3_3_203192331.py
@@ -1,20 +1,17 @@
 import os
-import inflect
-"""to use this module please download pip install inflect
-    Example answer: 'the' because it appears at least eleven times.
-    this module change integer to a word"""
 with open(os.getcwd()+'/'+'words.txt') as f:
-    text=f.read()
+    text=eval(f.read())
 f.close()
-mylist=eval(text)
 at_least_appears=5
 at_least_belongs=1
-p = inflect.engine()
-for item in mylist:
+mydict={}
+for item in text:
     if len(item[2])>at_least_belongs:
         if item[1]>=at_least_appears:
-            print("'{}' because it belongs to at least {} categories and because it appears at least {} times.".format(item[0],p.number_to_words(len(item[2])),p.number_to_words(item[1])))
+            mydict[item[0]]='because it belongs to at least two categories and because it appears at least five times.'
         else:
-            print("'{}' because it belongs to at least {} categories.".format(item[0],p.number_to_words(len(item[2]))))
+            mydict[item[0]]='because it belongs to at least tow categories.'
     elif item[1]>=at_least_appears:
-        print("'{}' because it appears at least {} times.".format(item[0],p.number_to_words(item[1])))
+        mydict[item[0]]='because it appears at least five times.'
+for k,v in mydict.items():
+    print(f"'{k}' {v}")
